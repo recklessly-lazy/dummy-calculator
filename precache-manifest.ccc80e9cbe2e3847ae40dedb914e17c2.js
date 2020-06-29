1cd8a7e4d451af81e4455d1897cde837df3badf2
Ramalakshmanan.S <srlaxman1497@gmail.com>
2020-06-29 18:18:16 +0530

    Updates

---
+++ precache-manifest.ccc80e9cbe2e3847ae40dedb914e17c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112e9b57a977f759faf31b824a23fa31",
+    "url": "/dummy-calculator/index.html"
+  },
+  {
+    "revision": "80383779fd90d66b0233",
+    "url": "/dummy-calculator/static/js/2.41179eb6.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/dummy-calculator/static/js/2.41179eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63ca7d9111662e4b4c0",
+    "url": "/dummy-calculator/static/js/main.47798d3f.chunk.js"
+  },
+  {
+    "revision": "34b13bc75d5a9ed90022",
+    "url": "/dummy-calculator/static/js/runtime-main.a724950e.js"
+  }
+]);
